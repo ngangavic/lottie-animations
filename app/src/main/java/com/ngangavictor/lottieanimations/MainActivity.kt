faeadb2fe26a038a41a7ef7048adeed3cbc94b99
ngangavic <ngangavictor10@gmail.com>
2021-07-19 12:56:37 +0300

    added menu

--- app/src/main/java/com/ngangavictor/lottieanimations/MainActivity.kt
+++ app/src/main/java/com/ngangavictor/lottieanimations/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.ngangavictor.lottieanimations
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_offline->{
+                startActivity(Intent(this,OfflineActivity::class.java))
+                finish()
+            }
+            R.id.action_online->{
+                Toast.makeText(this,"Uko hapa :-)",Toast.LENGTH_LONG).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
